a93b90d93124d3e223c9da61cb98aa3428114b63
Devin Mounts and Nikki Boyd <devin@>
2018-07-05 10:54:55 -0700

    create constructor to set majority of variables. Create Find method. All tests pass.

--- Tamagotchi.Tests/PetsTests.cs
+++ Tamagotchi.Tests/PetsTests.cs
@@ -1,13 +1,110 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Tamagotchi.Models;
 
 namespace Tamagotchi.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class PetsTests : IDisposable
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void Dispose()
         {
+            Pets.ClearAll();
         }
+
+        [TestMethod]
+        public void SetGetName_SetsGetsName_String()
+        {
+            Pets newPet = new Pets();
+
+            Assert.AreEqual("Sam", newPet.GetName());
+
+        }
+
+        //[TestMethod]
+        //public void SetGetId_SetsGetsId_Int()
+        //{
+        //    Pets newPet = new Pets();
+        //    newPet.SetId(1);
+        //    Assert.AreEqual(true, newPet.GetId());
+
+        //}
+
+        //[TestMethod]
+        //public void SetGetName_SetsGetsName_String()
+        //{
+            //Pets newPet = new Pets();
+            //newPet.SetName("Sam");
+            //Assert.AreEqual("Sam", newPet.GetName());
+
+        //}
+
+        [TestMethod]
+        public void SetGetAge_SetsGetsAge_Int()
+        {
+            Pets newPet = new Pets();
+
+            Assert.AreEqual(0, newPet.GetAge());
+
+        }
+
+        [TestMethod]
+        public void SetGetHunger_SetsGetsHunger_Int()
+        {
+            Pets newPet = new Pets();
+
+            Assert.AreEqual(5, newPet.GetHunger());
+
+        }
+
+        [TestMethod]
+        public void SetGetHappiness_SetsGetsHappines_Int()
+        {
+            Pets newPet = new Pets();
+           
+            Assert.AreEqual(5, newPet.GetHunger());
+        }
+
+        [TestMethod]
+        public void SetGetFatigue_SetsGetsFatigue_Int()
+        {
+            Pets newPet = new Pets();
+         
+            Assert.AreEqual(5, newPet.GetFatigue());
+        }
+        
+        [TestMethod]
+        public void SetGetStatus_SetsGetsStatus_String()
+        {
+            Pets newPet = new Pets();
+
+            Assert.AreEqual(true, newPet.GetStatus());
+        }
+
+        [TestMethod]
+        public void AddGetPets_AddGetList_List()
+        {
+            Pets newPet = new Pets();
+
+            Pets newPet02 = new Pets();
+
+            List<Pets> compare = new List<Pets>() { newPet, newPet02 };
+            CollectionAssert.AreEqual(compare, Pets.GetAll());
+        }
+
+        [TestMethod]
+        public void SetGetId_SetsGetsId_int()
+        {
+            Pets newPet = new Pets();
+
+            Pets newPet02 = new Pets();
+
+            List<Pets> compare = new List<Pets>() { newPet, newPet02 };
+
+            Assert.AreEqual(newPet, Pets.Find(1));
+
+        }
+
     }
 }
--- Tamagotchi/Models/Pets.cs
+++ Tamagotchi/Models/Pets.cs
@@ -1,10 +1,129 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Tamagotchi.Models
 {
     public class Pets
     {
+        private string _name;
+        private int _id;
+        private static int _age;
+        private int _hunger;
+        private int _happiness;
+        private int _fatigue;
+        private bool _alive;
+        private static List<Pets> _pets = new List<Pets> { };
+
+        public Pets(string name)
+        {
+            _name = name;
+            _pets.Add(this);
+            _id = _pets.Count;
+            _age = 0;
+            _hunger = 5;
+            _happiness = 5;
+            _fatigue = 5;
+            _alive = true;
+        }
+
         public Pets()
         {
+            _name = "Sam";
+            _pets.Add(this);
+            _id = _pets.Count;
+            _age = 0;
+            _hunger = 5;
+            _happiness = 5;
+            _fatigue = 5;
+            _alive = true;
+        }
+
+
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        //public void SetName(string newName)
+        //{
+        //    _name = newName;
+        //}
+
+        public static Pets Find(int searchId)
+        {
+            return _pets[searchId -1];
+        }
+
+        //public void SetId(List<Pets> allPets)
+        //{
+        //    _id = allPets.Count - 1;
+        //}
+
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        //public void SetAge(int petAge)
+        //{
+        //    _age = petAge;
+        //}
+
+        public int GetHunger()
+        {
+            return _hunger;
+        }
+
+        //public void SetHunger(int hunger)
+        //{
+        //    _hunger = hunger;
+        //}
+
+        public int GetHappiness()
+        {
+            return _happiness;
+        }
+
+        //public void SetHappiness(int happiness)
+        //{
+        //    _happiness = happiness;
+        //}
+
+        public int GetFatigue()
+        {
+            return _fatigue;
+        }
+
+        //public void SetFatigue(int fatigue)
+        //{
+        //    _fatigue = fatigue;
+        //}
+               
+        public bool GetStatus()
+        {
+            return _alive;
+        }
+
+        //public void SetStatus(string status)
+        //{
+        //    _alive = status;
+        //}
+       
+
+        public static List<Pets> GetAll()
+        {
+           return _pets;
+            //return false;
+        }
+
+        //public void AddPet()
+        //{
+        //    _pets.Add(this);
+        //}
+        public static void ClearAll()
+        {
+            _pets.Clear();
         }
     }
 }
